ab12f5375f74bfeeead178e2fa9fc24b74c9e0d6
PavelShpagin <pavelandrewshpagin@gmail.com>
2025-08-02 18:40:26 +0000

    fix requirements

---
+++ setup.sh
@@ -0,0 +1,5 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install --no-cache-dir torch torchvision --index-url https://download.pytorch.org/whl/cpu
+pip install -r requirements.txt
